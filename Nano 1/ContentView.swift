f1671d0a3fe458c16e459735e3af86e14239db2d
jasminisantos <jasminirebeca10@gmail.com>
2023-07-25 21:25:12 -0300

    Initial Commit

---
+++ Nano 1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Nano 1
+//
+//  Created by Jasmini Rebecca Gomes dos Santos on 25/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Nano 1/Nano_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_1App.swift
+//  Nano 1
+//
+//  Created by Jasmini Rebecca Gomes dos Santos on 25/07/23.
+//
+
+import SwiftUI
+
+@main
+struct Nano_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
