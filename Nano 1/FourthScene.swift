b7d3f6b4cd27b158201f9d156691e1123e27f37e
jasminisantos <jasminirebeca10@gmail.com>
2023-08-02 21:14:17 -0300

    creating fourth scene

--- Nano 1/FourthScene.swift
+++ Nano 1/FourthScene.swift
@@ -1,5 +1,5 @@
 //
-//  ThirdScene.swift
+//  FourthScene.swift
 //  Nano 1
 //
 //  Created by Jasmini Rebecca Gomes dos Santos on 27/07/23.
@@ -7,21 +7,15 @@
 
 import SwiftUI
 import SpriteKit
-class ThirdScene: SceneModel {
+class FourthScene: SceneModel {
     weak var gameManager: GameManager?
     
     var table: SKSpriteNode!
+    var pie: SKSpriteNode!
+    var i = 1;
     
     override func didMove(to view: SKView) {
-        setupLayer("RedRoomBackground", 1)
-        setupTable()
-        setupLeftArrow()
-        setupRightArrow()
-        
-        if actionHappened(actionName: "FoodTableScene"){
-            self.table.texture = SKTexture(imageNamed: "EmptyTable")
-        }
-        
+        setupNormalMode()
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?)
     {
@@ -33,14 +27,21 @@ class ThirdScene: SceneModel {
         {
             if name == "RightArrow"
             {
-                gameManager?.goToScene(.scene4)
+                gameManager?.goToScene(.scene5)
             }
             else if name == "LeftArrow"
             {
-                gameManager?.goToScene(.scene2)
+                gameManager?.goToScene(.scene3)
             }
             else if name == "FoodTable"{
-                gameManager?.goToScene(.foodTable)
+                setupCheckingObjectMode()
+            }
+            else if name == "DownArrow"
+            {
+                setupNormalMode()
+            }
+            else if name.contains("Pie") && !name.contains("Message"){
+                changePieTexture()
             }
         }
 
@@ -57,4 +58,82 @@ class ThirdScene: SceneModel {
         self.addChild(table)
     }
     
+    func setupNormalMode(){
+        self.children.forEach { node in
+            if node.name != nil {
+                node.removeFromParent()
+            }
+        }
+        setupLayer("RedRoomBackground", 1)
+        setupTable()
+        setupLeftArrow()
+        setupRightArrow()
+        
+        if actionHappened(actionName: "MessageFound"){
+            self.table.texture = SKTexture(imageNamed: "EmptyTable")
+        }
+    }
+    
+    func setupCheckingObjectMode(){
+        self.children.forEach { node in
+            if node.name != nil {
+                node.removeFromParent()
+            }
+        }
+        setupLayer("Floor", 1)
+        setupEmptyTable()
+        setupPie()
+        setupDownArrow()
+        
+        if actionHappened(actionName: "MessageFound"){
+            self.pie.texture = SKTexture(imageNamed: "PieMessage")
+            self.pie.name = "PieMessage"
+        }
+    }
+    
+    func setupEmptyTable(){
+        table = SKSpriteNode(imageNamed: "FoodTableUpEmpty")
+        table.position = CGPoint(x: frame.width*0.5, y: frame.height*0.5)
+        table.zPosition = 1
+        table.name = "FoodTableUpEmpty"
+        
+        let tableBody = SKPhysicsBody(rectangleOf: CGSize(width: table.size.width, height: table.size.height))
+        tableBody.isDynamic = false
+        table.physicsBody = tableBody
+        self.addChild(table)
+    }
+    func setupPie(){
+        if !actionHappened(actionName: "MessageFound"){
+            pie = SKSpriteNode(imageNamed: "Pie\(i)")
+            pie.name = "Pie\(i)"
+        }
+        else {
+            pie = SKSpriteNode(imageNamed: "PieMessage")
+            pie.name = "PieMessage"
+        }
+        
+        pie.position = CGPoint(x: frame.width*0.5, y: frame.height*0.5)
+        pie.zPosition = 2
+        
+        let pieBody = SKPhysicsBody(rectangleOf: CGSize(width: pie.size.width, height: pie.size.height))
+        pieBody.isDynamic = false
+        pie.physicsBody = pieBody
+        self.addChild(pie)
+    }
+    func changePieTexture() {
+        if actionHappened(actionName: "MessageFound") {
+            self.pie.texture = SKTexture(imageNamed: "PieMessage")
+            self.pie.name = "PieMessage"
+        }
+        else {
+            i += 1
+            if i < 5 {
+                self.pie.texture = SKTexture(imageNamed: "Pie\(i)")
+            }
+            if i == 5 {
+                setAction(actionName: "MessageFound")
+                self.pie.texture = SKTexture(imageNamed: "PieMessage")
+            }
+        }
+    }
 }
